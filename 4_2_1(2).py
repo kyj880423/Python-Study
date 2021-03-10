52c09566477fb3c52a3488f792c923199b5d8e55
kyj880423 <callakrsos@naver.com>
2021-03-11 08:36:18 +0900

    fix

--- 4_2_1(2).py
+++ 4_2_1(2).py
@@ -12,20 +12,34 @@ krx_list.종목코드 = krx_list.종목코드.map('{:06d}'.format)
 df = krx_list.sort_values(by='종목코드')
 
 
-print(df[df["회사명"].str.contains('카카오')])
+series = df[df["회사명"].str.contains('삼성바이오')]
+print(series)
 
 
+cname = series["회사명"].values[0] 
+code = series["종목코드"].values[0] 
+# Series type.
+#print(type( df[df["회사명"].str.contains('삼성바이오')] ))
+#print( type(values))
 
-last_page = 523
+print("회사명 : {} , 종목코드 : {}".format(cname, code))
+
+#values = df[df["회사명"].str.contains('삼성바이오')]["종목코드"].values
+
+
+last_page = 107
 headers = {
 "User-Agent":"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 }
 
 
+
+
+
 df = pd.DataFrame()
-sise_url = "https://finance.naver.com/item/sise_day.nhn?code=035720"
+sise_url = "https://finance.naver.com/item/sise_day.nhn?"
 for page in range(1, int(last_page) + 1):
-    page_url = "{}&page={}".format(sise_url, page)
+    page_url = "{}code={}&page={}".format(sise_url, code, page)
     print(page_url)
 
     r = requests.get(page_url, headers=headers)
@@ -34,8 +48,7 @@ for page in range(1, int(last_page) + 1):
 
 df.dropna()
 
-df.to_csv("sise_kakao.csv")
-
+df.to_csv( "{}.csv".format(cname) )
 
 
 
